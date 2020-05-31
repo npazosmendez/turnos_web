e9bb5adab9b8772ba85775b415e12e1a4ceac409
Nicolás Pazos <npazosmendez@gmail.com>
2020-05-31 14:17:23 -0300

    Saca envars de npm start

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.port = process.env.PORT || 3443;
+config.port = process.env.PORT || 3000;
 config.https = Boolean(process.env.HTTPS) || false;
 config.ssl_cert = "/etc/ssl/certs/turnos.cert";
 config.ssl_private = "/etc/ssl/private/turnos.key";
