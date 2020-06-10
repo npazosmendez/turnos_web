0de16a3c39ec53f7e2c0c2a9267ea88f1c0b38d3
Martin Rados <radosm@gmail.com>
2020-06-10 17:39:07 -0300

    alambrado

--- frontend/web/whatsapp.js
+++ frontend/web/whatsapp.js
@@ -1,6 +1,10 @@
 function whatsapp(port,mensaje,htmlFile){
     var prot = window.location.protocol;
     var host = window.location.hostname;
+
+    if (port=="0") {
+        port=window.location.port.toString();
+    }
     var result = prot + "//" + host + ":" + port + "/tmp/" + encodeURIComponent(htmlFile);
     window.open("http://web.whatsapp.com/send?text="+mensaje+result,"compartirwhatsapp");
 }
