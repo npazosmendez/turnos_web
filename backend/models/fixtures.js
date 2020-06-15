4732e93b17906c4137d50ea2e474d9f4d113cd29
Nicolás Pazos <npazosmendez@gmail.com>
2020-06-15 16:24:03 -0300

    Agrega imágenes al fixture

--- backend/models/fixtures.js
+++ backend/models/fixtures.js
@@ -17,6 +17,7 @@ db.sync({ force: true })
           latitud: -34.6168142,
           longitud: -58.3727203,
           usuarioId: elver.id,
+          pathImagen: "imagenes_conceptos/verduleria.jpg"
         },
         {
           habilitado: true,
@@ -25,6 +26,7 @@ db.sync({ force: true })
           latitud: -34.6176142,
           longitud: -58.3721203,
           usuarioId: elver.id,
+          pathImagen: "imagenes_conceptos/zapateria.jpg"
         },
         {
           habilitado: false,
@@ -33,6 +35,7 @@ db.sync({ force: true })
           latitud: -34.6176142,
           longitud: -58.3621203,
           usuarioId: elver.id,
+          pathImagen: "imagenes_conceptos/merceria.jpg"
         },
       ]);
 
@@ -47,6 +50,7 @@ db.sync({ force: true })
           longitud: -58.3919203,
           usuarioId: jose.id,
           maximaEspera: 15,
+          pathImagen: "imagenes_conceptos/mcdonalds.png"
         },
         {
           habilitado: true,
@@ -55,6 +59,7 @@ db.sync({ force: true })
           latitud: -34.6032899,
           longitud: -58.4130282,
           usuarioId: jose.id,
+          pathImagen: "imagenes_conceptos/starbucks.png"
         },
         {
           habilitado: true,
@@ -63,6 +68,7 @@ db.sync({ force: true })
           latitud: -34.5883421,
           longitud: -58.4886747,
           usuarioId: jose.id,
+          pathImagen: "imagenes_conceptos/farmacity.png"
         },
         {
           habilitado: true,
@@ -71,6 +77,7 @@ db.sync({ force: true })
           latitud: -34.6194593,
           longitud: -58.4766261,
           usuarioId: jose.id,
+          pathImagen: "imagenes_conceptos/coto.jpg"
         },
       ]);
 
@@ -84,6 +91,7 @@ db.sync({ force: true })
           latitud: -34.6050228,
           longitud: -58.4070207,
           usuarioId: asmid.id,
+          pathImagen: "imagenes_conceptos/telas.jpg"
         },
         {
           habilitado: true,
@@ -92,6 +100,7 @@ db.sync({ force: true })
           latitud: -34.5601519,
           longitud: -58.4631785,
           usuarioId: asmid.id,
+          pathImagen: "imagenes_conceptos/burgio.jpg"
         },
         {
           habilitado: true,
@@ -100,6 +109,7 @@ db.sync({ force: true })
           latitud: -34.5832419,
           longitud: -58.4857741,
           usuarioId: asmid.id,
+          pathImagen: "imagenes_conceptos/patagonia.jpg"
         },
       ]);
 
@@ -113,6 +123,7 @@ db.sync({ force: true })
           latitud: -34.5555884,
           longitud: -58.4241203,
           usuarioId: rober.id,
+          pathImagen: "imagenes_conceptos/bondiola.jpg"
         },
       ]);
 
