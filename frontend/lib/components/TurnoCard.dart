9b98cb88366fa5ef9bb8a4aa0dfcdbef5ab437b8
Martin Rados <radosm@gmail.com>
2020-06-16 15:48:19 -0300

    mejor visualización lista de turnos del cliente en mobile

--- frontend/lib/components/TurnoCard.dart
+++ frontend/lib/components/TurnoCard.dart
@@ -42,58 +42,57 @@ class TurnoCard extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget card = Card(
       child: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: Text(
-                "${turno.concepto.nombre.toUpperCase()} (#${turno.numeroToDisplay})",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blue),
+        child: 
+          Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                child:
+                  Text(
+                    "${turno.concepto.nombre.toUpperCase()} (#${turno.numeroToDisplay})",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blue),
+                  ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                     padding: EdgeInsets.fromLTRB(0, 0, 100, 0),
-                     child: 
+              Padding(
+                padding: EdgeInsets.all(5),
+                child: 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
                       PersonasAdelante(personasAd),
-                  ),
-                  Column(
-                    children: [
-                      BotonConIcono(
-                        icon: Icon(Icons.access_time, color: Colors.white),
-                        text: "ESTOY ATRASADO",
-                        tooltip: "Dejá pasar a quien esté detrás tuyo",
-                        onPressed: () => this.tryDejarPasar(context),
+                      Spacer(),
+                      Column(
+                        children: [
+                          BotonConIcono(
+                            icon: Icon(Icons.access_time, color: Colors.white),
+                            text: "ESTOY ATRASADO",
+                            tooltip: "Dejá pasar a quien esté detrás tuyo",
+                            onPressed: () => this.tryDejarPasar(context),
+                          ),
+                          BotonConIcono(
+                            icon: FaIcon(FontAwesomeIcons.qrcode, color: Colors.white),
+                            text: "CÓDIGO QR",
+                            tooltip: "Mostrá el código QR a tu comerciante amigo",
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) => TurnoQrDialog(turno),
+                              );
+                            },
+                          ),
+                          BotonConIcono(
+                            icon: FaIcon(FontAwesomeIcons.whatsapp, color: Colors.white),
+                            text: "COMPARTIR",
+                            tooltip: "Compartí el turno por Whatsapp",
+                            onPressed: compartirPorWpp
+                          ),
+                        ],
                       ),
-                      BotonConIcono(
-                        icon: FaIcon(FontAwesomeIcons.qrcode, color: Colors.white),
-                        text: "CÓDIGO QR",
-                        tooltip: "Mostrá el código QR a tu comerciante amigo",
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) => TurnoQrDialog(turno),
-                          );
-                        },
-                      ),
-                      BotonConIcono(
-                        icon: FaIcon(FontAwesomeIcons.whatsapp, color: Colors.white),
-                        text: "COMPARTIR",
-                        tooltip: "Compartí el turno por Whatsapp",
-                        onPressed: compartirPorWpp
-                      ),
-                    ],
-                  ),
-                ],
-              )
-            ),
-          ],
-        ),
+                    ]
+                 )
+              ),
+            ]
+          )
       )
     );
     return card;
